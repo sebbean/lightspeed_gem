4ef72508cd99b624ef500787743c96287ca4670d
Sebastian Bean <sebastian@ginlanemedia.com>
2013-09-23 11:12:37 -0400

    updated a bit i think

--- lib/lightspeed.rb
+++ lib/lightspeed.rb
@@ -11,21 +11,18 @@ module Lightspeed
     headers 'Accept-Encoding'  => "deflate, gzip"
 
     @configured = false
-
     def self.configure(config)
-
       base_uri "#{config[:endpoint]}/api/"
       basic_auth config[:username], config[:password]
       headers 'User-Agent'       => config[:user_agent]
       headers 'X-PAPPID'         => config[:app_id]
       headers 'Cookie'           => config[:cookie] unless config[:cookie].nil?
       @configured = true
-
-      puts "configggging"
     end
 
   	def self.load_config_file(path="lightspeed.yml")
-      raw_config = File.read(path)
+      base = Rails.root || '.'
+      raw_config = File.read(File.join(base, 'config', path))
 	    YAML.load(raw_config)[Rails.env].symbolize_keys 
   	end
 
@@ -41,16 +38,13 @@ module Lightspeed
       table = self.to_s.downcase.split('::').last
       path = "/#{table}s#{path}" 
 
-      puts path
-      options = default_options.dup.merge(options)
-      process_cookies(options)
-
-      resp = Request.new(http_method, path, options).perform(&block)
-    
+      resp = super http_method, path, options, &block
       resp = resp.parsed_response.values.first if resp.parsed_response.keys.count == 1
-      resp = resp.values.first if resp.keys.count == 1
+      return resp unless resp 
+      resp = resp.values.first if resp.keys.count == 1 
+      return resp if resp.kind_of?(Array)
+      resp = resp.values.first if resp.keys.count == 2 && resp.keys.last.match('total_count')
 
-      # headers 'Cookie' => resp.headers['set-cookie'].first unless @@configured
       resp
     end
 
@@ -65,15 +59,24 @@ module Lightspeed
     def self.find(id=1)
       get "/#{id}/"
     end
+
     def self.last
       get '/', query: {count: 1}
+    end
+
+    def self.since(time=nil)
+      time ||= 1.days.ago
+      filter "date_mod > \"#{time}\""
+    end
+
+    def self.filter(query)
+      get '/', query: {filter: "(#{query})"}
     end
   end
 
   class Product < Api
     def self.find_by_code(code)
-      filter = "(code contains[cd] \"#{code}\")"
-      get '/', query: {filter: filter}
+      filter "code contains[cd] \"#{code}\""
     end 
   end
 
